29adaf870c0f471e5f8cf4a20f0d5be6c1c237c0
Anth Riv <anthony.rivoiron@gmail.com>
2022-09-21 11:00:46 +0200

    first commit

---
+++ src/features/transactions/transactionsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
+const initialState = Object.fromEntries(CATEGORIES.map(category => [category, []]))
+
+const transactionsSlice = createSlice({
+  name: 'transactions',
+  initialState: initialState,
+  reducers: {
+    addTransaction: (state, action) => void(state[action.payload.category].push(action.payload)),
+    deleteTransaction: (state, action)=>{
+      const index = state[action.payload.category].findIndex(obj => obj.id === action.payload.id)
+      state[action.payload.category].splice(index, 1);
+    }
+  }
+})
+
+export const selectTransactions = (state) => state.transactions;
+export const selectFlattenedTransactions = (state) => Object.values(state.transactions).reduce((a,b) => [...a, ...b], []);
+
+export const { addTransaction, deleteTransaction } = transactionsSlice.actions;
+export default transactionsSlice.reducer;
+
